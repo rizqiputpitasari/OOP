2435aa9f5166bc44265dd330de9fc38a8bd2aee5
rizqiputpitasari <48272933+rizqiputpitasari@users.noreply.github.com>
2019-03-06 14:23:23 +0700

    Create rizqi_putpitasari_17090075_4B.java

---
+++ rizqi_putpitasari_17090075_4B.java
@@ -0,0 +1,7 @@
+package tugas;
+
+public class datang {
+    public static void main(String[] args){
+        system.out.println("Selamat datang di Object Oriented Programming \n rizqi_putpitasari_17090075_4B");
+    }
+}
